775d75d6c78ebca0dfccd6161266ee895a1e85f2
robneu <rob@wpsitecare.com>
2015-04-12 22:18:50 -0500

    Removed footer credits

--- footer.php
+++ footer.php
@@ -15,25 +15,13 @@
 
 		<footer <?php hybrid_attr( 'footer' ); ?>>
 
-			<?php tha_footer_top(); ?>
-
 			<div <?php hybrid_attr( 'wrap', 'footer' ); ?>>
 
-				<p class="credit">
-					<?php
-					printf(
-						// Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link.
-						__( 'Copyright &#169; %1$s %2$s. Powered by %3$s.', 'alpha' ),
-						date_i18n( 'Y' ),
-						hybrid_get_site_link(),
-						hybrid_get_wp_link()
-					);
-					?>
-				</p><!-- .credit -->
+				<?php tha_footer_top(); ?>
+
+				<?php tha_footer_bottom(); ?>
 
 			</div><!-- .wrap -->
-
-			<?php tha_footer_bottom(); ?>
 
 		</footer><!-- .footer -->
 
