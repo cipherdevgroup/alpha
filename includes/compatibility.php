f72124a8baf4397751915833e797c62164faf86f
robneu <rob@wpsitecare.com>
2015-08-05 03:11:29 -0500

    Added compat for RP4WP

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -1,20 +1,26 @@
 <?php
 /**
- * Plugin Compatibility File
+ * Provides compatibility with various WordPress plugins.
  *
- * @package     Alpha
- * @subpackage  CareLib
- * @copyright   Copyright (c) 2015, WP Site Care, LLC
- * @license     GPL-2.0+
- * @since       1.0.0
+ * @package    Alpha\Functions\Compatibility
+ * @subpackage CareLib
+ * @copyright  Copyright (c) 2015, WP Site Care, LLC
+ * @license    GPL-2.0+
+ * @since      1.0.0
  */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) or exit;
 
 add_action( 'after_setup_theme', 'alpha_jetpack_setup', 12 );
 /**
  * Make adjustments to the theme when Jetpack is installed and activated.
+ * Change the default related posts image size.
  *
  * @since  1.0.0
  * @return void
+ * @access public
+ * @return string
  */
 function alpha_jetpack_setup() {
 	// Return early if Jetpack isn't activated.
@@ -27,4 +33,16 @@ function alpha_jetpack_setup() {
 		'container' => 'main',
 		'footer'    => 'page',
 	) );
+}
+
+add_filter( 'rp4wp_thumbnail_size', 'alpha_related_posts_image' );
+/**
+ * Change the default related posts image size.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string
+ */
+function alpha_related_posts_image() {
+	return 'related-posts';
 }
