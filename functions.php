61a4b2c21490a3d42e03e389170a1c05844aa647
robneu <rob@wpsitecare.com>
2015-08-05 03:12:42 -0500

    Updated functions

--- functions.php
+++ functions.php
@@ -9,8 +9,19 @@
  * @since       1.0.0
  */
 
+/**
+ * Return a path to the alpha includes directory with a trailing slash.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string
+ */
+function alpha_get_includes_dir() {
+	return trailingslashit( get_template_directory() ) . 'includes/';
+}
+
 // Include CareLib.
-require_once trailingslashit( get_template_directory() ) . 'includes/vendor/carelib/carelib.php';
+require_once alpha_get_includes_dir() . 'vendor/carelib/carelib.php';
 carelib()->run( array( 'prefix' => 'alpha' ) );
 
 add_action( 'after_setup_theme', 'alpha_setup', 10 );
@@ -33,9 +44,47 @@ function alpha_setup() {
 
 	add_theme_support( 'automatic-feed-links' );
 
+	add_theme_support( 'site-logo' );
+
 	register_nav_menus( array(
 		'primary'   => _x( 'Primary Menu', 'nav menu location', 'alpha' ),
 		'secondary' => _x( 'Secondary Menu', 'nav menu location', 'alpha' ),
+	) );
+}
+
+add_action( 'init', 'alpha_register_image_sizes', 5 );
+/**
+ * Register custom image sizes for the theme.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_register_image_sizes() {
+	set_post_thumbnail_size( 175, 130, true );
+
+	add_image_size( 'alpha-full',   1025, 500, true );
+	add_image_size( 'related-posts', 350, 250, true );
+}
+
+add_action( 'widgets_init', 'alpha_register_sidebars', 5 );
+/**
+ * Registers sidebars.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_register_sidebars() {
+	carelib_class( 'sidebar' )->register( array(
+		'id'          => 'primary',
+		'name'        => _x( 'Primary Sidebar', 'sidebar', 'alpha' ),
+		'description' => __( 'The main sidebar. It is displayed on either the left or right side of the page based on the chosen layout.', 'alpha' ),
+	) );
+	carelib_class( 'sidebar' )->register( array(
+		'id'          => 'footer-widgets',
+		'name'        => _x( 'Footer Widgets', 'sidebar', 'alpha' ),
+		'description' => __( 'A widgeted area which displays just before the main site footer on all pages.', 'alpha' ),
 	) );
 }
 
@@ -47,13 +96,26 @@ add_action( 'after_setup_theme', 'alpha_includes', 10 );
  * @return  void
  */
 function alpha_includes() {
-	$dir = trailingslashit( get_template_directory() ) . 'includes/';
+	$dir = alpha_get_includes_dir();
 
 	require_once "{$dir}vendor/tha-theme-hooks.php";
+	require_once "{$dir}attributes.php";
 	require_once "{$dir}compatibility.php";
-	require_once "{$dir}general.php";
-	require_once "{$dir}scripts.php";
-	require_once "{$dir}widgetize.php";
+	require_once "{$dir}helpers-comments.php";
+	require_once "{$dir}layout.php";
+
+	if ( is_admin() ) {
+		require_once "{$dir}admin/scripts.php";
+	} else {
+		require_once "{$dir}helpers-blog.php";
+		require_once "{$dir}helpers-entry.php";
+		require_once "{$dir}helpers-global.php";
+		require_once "{$dir}hooked-archive.php";
+		require_once "{$dir}hooked-attachment.php";
+		require_once "{$dir}hooked-entry.php";
+		require_once "{$dir}hooked-global.php";
+		require_once "{$dir}scripts.php";
+	}
 }
 
 // Add a hook for child themes to execute code.
