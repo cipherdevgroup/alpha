35f97969dba286b2878c1c2fd0a148ba518c834c
robneu <rob@wpsitecare.com>
2015-09-13 03:11:31 -0500

    Indentation

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
-* The main template file.
-*
-* This is here for WordPress theme compatibility. The actual template structure
-* can be found in `/templates/framework.php`
-*
-* To modify the markup, you can filter `alpha_index_template` to load a custom
-* framework file rather than the default `framework.php`.
+ * The main template file.
+ *
+ * This is here for WordPress theme compatibility. The actual template structure
+ * can be found in `/templates/framework.php`
+ *
+ * To modify the markup, you can filter `alpha_index_template` to load a custom
+ * framework file rather than the default `framework.php`.
  *
  * @since      1.0.0
  * @package    Alpha\Templates
