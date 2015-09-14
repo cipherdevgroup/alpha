109b2047af420cc8798547c4588d5ca653c16924
robneu <rob@wpsitecare.com>
2015-09-14 15:38:42 -0500

    Added helper functions for forcing the layout

--- includes/layout.php
+++ includes/layout.php
@@ -54,17 +54,6 @@ function alpha_register_layouts() {
 }
 
 /**
- * Return the narrow full width content slug.
- *
- * @since  1.0.0
- * @access public
- * @return string the slug of the full width content layout.
- */
-function alpha_return_full_width_narrow_layout() {
-	return '1c-narrow';
-}
-
-/**
  * Return the full width content slug.
  *
  * @since  1.0.0
@@ -73,6 +62,41 @@ function alpha_return_full_width_narrow_layout() {
  */
 function alpha_return_full_width_layout() {
 	return '1c';
+}
+
+/**
+ * Force the full width layout and return the slug.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string the slug of the full content layout.
+ */
+function alpha_force_full_width_layout() {
+	add_filter( 'alpha_allow_layout_control', '__return_false' );
+	return alpha_return_full_width_layout();
+}
+
+/**
+ * Return the narrow narrow full width content slug.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string the slug of the narrow full width content layout.
+ */
+function alpha_return_full_narrow_layout() {
+	return '1c-narrow';
+}
+
+/**
+ * Force the narrow full width layout and return the slug.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string the slug of the narrow full content layout.
+ */
+function alpha_force_full_narrow_layout() {
+	add_filter( 'alpha_allow_layout_control', '__return_false' );
+	return alpha_return_full_narrow_layout();
 }
 
 /**
@@ -87,6 +111,18 @@ function alpha_return_right_sidebar_layout() {
 }
 
 /**
+ * Force the 2 column content / sidebar layout slug.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string the slug of the right sidebar layout.
+ */
+function alpha_force_right_sidebar_layout() {
+	add_filter( 'alpha_allow_layout_control', '__return_false' );
+	return alpha_return_right_sidebar_layout();
+}
+
+/**
  * Return the 2 column sidebar / content layout slug.
  *
  * @since  1.0.0
@@ -95,4 +131,16 @@ function alpha_return_right_sidebar_layout() {
  */
 function alpha_return_left_sidebar_layout() {
 	return '2c-l';
+}
+
+/**
+ * Force the 2 column content / sidebar layout slug.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string the slug of the right sidebar layout.
+ */
+function alpha_force_left_sidebar_layout() {
+	add_filter( 'alpha_allow_layout_control', '__return_false' );
+	return alpha_return_left_sidebar_layout();
 }
