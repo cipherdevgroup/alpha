e73807f3f3197a470ebc0415dbb4c2c045cc70fe
robneu <rob@wpsitecare.com>
2015-10-09 15:17:15 -0500

    Replaced archive header template with custom action
    This is more consistent with the rest of the theme and allows for more
    flexibility.

--- includes/hooked-archive.php
+++ includes/hooked-archive.php
@@ -12,20 +12,90 @@
 // Prevent direct access.
 defined( 'ABSPATH' ) || exit;
 
-add_action( 'tha_content_top',         'alpha_archive_header',    12 );
-add_action( 'tha_content_while_after', 'alpha_posts_navigation',  10 );
+add_action( 'tha_content_top',         'alpha_archive_header',        12 );
+add_action( 'alpha_archive_header',    'alpha_archive_header_open',    5 );
+add_action( 'alpha_archive_header',    'alpha_wrap_open',             10 );
+add_action( 'alpha_archive_header',    'alpha_archive_title',         15 );
+add_action( 'alpha_archive_header',    'alpha_breadcrumbs',           20 );
+add_action( 'alpha_archive_header',    'alpha_archive_sub_terms',     25 );
+add_action( 'alpha_archive_header',    'alpha_archive_description',   30 );
+add_action( 'alpha_archive_header',    'alpha_wrap_close',            95 );
+add_action( 'alpha_archive_header',    'alpha_archive_header_close', 100 );
+add_action( 'tha_content_while_after', 'alpha_posts_navigation',      10 );
 
 /**
- * Load the loop meta template.
+ * Add a custom action for the archive header.
  *
  * @since  1.0.0
  * @access public
  * @return void
  */
 function alpha_archive_header() {
-	if ( is_archive() || is_search() ) {
-		get_template_part( 'template-parts/archive-header' );
+	if ( alpha_is_archive() ) {
+		do_action( 'alpha_archive_header' );
 	}
+}
+
+/**
+ * Output the opening markup for the archive header.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_archive_header_open() {
+	echo '<div ' . alpha_get_attr( 'archive-header' ) . '>';
+}
+
+/**
+ * Output the archive title.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_archive_title() {
+	echo '<h1 ' . alpha_get_attr( 'archive-title' ) . '>' . get_the_archive_title() . '</h1>';
+}
+
+/**
+ * Load the archive sub-terms menu template.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_archive_sub_terms() {
+	if ( is_category() || is_tax() ) {
+		get_template_part( 'template-parts/menu/sub-terms' );
+	}
+}
+
+/**
+ * Output the archive description.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_archive_description() {
+	if ( ! is_paged() && $desc = get_the_archive_description() ) {
+		printf( '<div %s>%s</div><!-- .archive-description -->',
+			alpha_get_attr( 'archive-description' ),
+			$desc
+		);
+	}
+}
+
+/**
+ * Output the closing markup for the archive header.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function alpha_archive_header_close() {
+	echo '</div><!-- .archive-header -->';
 }
 
 /**
