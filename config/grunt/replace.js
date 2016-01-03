fbcfe353ac5705ef17854cad752389fbae41ff0e
robneu <rob@wpsitecare.com>
2016-01-03 02:10:46 -0500

    Removed unused tasks

--- config/grunt/replace.js
+++ config/grunt/replace.js
@@ -1,40 +1,5 @@
 // https://github.com/outaTiME/grunt-replace
 module.exports = {
-	style: {
-		options: {
-			patterns: [
-				{
-					// Add line break between banner and minified
-					match: /\*\/(?=\S)/g,
-					replacement: '*/\n'
-				}
-			]
-		},
-		files: [{
-			expand: true,
-			src: [
-				'<%= paths.tmp %>style*.min.css'
-			]
-		}]
-	},
-	release: {
-		options: {
-			patterns: [
-				{
-					match: 'release',
-					replacement: '<%= pkg.version %>'
-				}
-			]
-		},
-		files: [
-			{
-				expand: true,
-				src: [
-					'**/*'
-				]
-			}
-		]
-	},
 	// Useful when forking this project into a new project
 	packagename: {
 		options: {
