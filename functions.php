401d87307b8ff228593c2c3b9d1fa0fd6afe9a80
robneu <rob@wpsitecare.com>
2016-01-19 14:04:20 -0500

    Standardized constant name

--- functions.php
+++ functions.php
@@ -18,24 +18,24 @@ defined( 'ABSPATH' ) || exit;
  * @uses  get_template_directory()
  * @uses  trailingslashit()
  */
-define( 'ALPHA_DIR',  trailingslashit( get_template_directory() ) );
+define( 'PARENT_THEME_DIR',  trailingslashit( get_template_directory() ) );
 
-require_once ALPHA_DIR . 'includes/vendor/carelib/carelib.php';
-require_once ALPHA_DIR . 'includes/vendor/tha-theme-hooks.php';
-require_once ALPHA_DIR . 'includes/attributes.php';
-require_once ALPHA_DIR . 'includes/plugins.php';
-require_once ALPHA_DIR . 'includes/scripts.php';
-require_once ALPHA_DIR . 'includes/template-archive.php';
-require_once ALPHA_DIR . 'includes/template-attachment.php';
-require_once ALPHA_DIR . 'includes/template-entry.php';
-require_once ALPHA_DIR . 'includes/template-global.php';
-require_once ALPHA_DIR . 'includes/template-layout.php';
-require_once ALPHA_DIR . 'includes/theme-setup.php';
-require_once ALPHA_DIR . 'includes/actions.php';
-require_once ALPHA_DIR . 'includes/filters.php';
+require_once PARENT_THEME_DIR . 'includes/vendor/carelib/carelib.php';
+require_once PARENT_THEME_DIR . 'includes/vendor/tha-theme-hooks.php';
+require_once PARENT_THEME_DIR . 'includes/attributes.php';
+require_once PARENT_THEME_DIR . 'includes/plugins.php';
+require_once PARENT_THEME_DIR . 'includes/scripts.php';
+require_once PARENT_THEME_DIR . 'includes/template-archive.php';
+require_once PARENT_THEME_DIR . 'includes/template-attachment.php';
+require_once PARENT_THEME_DIR . 'includes/template-entry.php';
+require_once PARENT_THEME_DIR . 'includes/template-global.php';
+require_once PARENT_THEME_DIR . 'includes/template-layout.php';
+require_once PARENT_THEME_DIR . 'includes/theme-setup.php';
+require_once PARENT_THEME_DIR . 'includes/actions.php';
+require_once PARENT_THEME_DIR . 'includes/filters.php';
 
 if ( is_admin() ) {
-	require_once ALPHA_DIR . 'admin/layout.php';
+	require_once PARENT_THEME_DIR . 'admin/layout.php';
 }
 
 carelib()->set_prefix( 'alpha' )->run();
