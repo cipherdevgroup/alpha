d211de698f9ca82a76e7c74f3884f354f1e6d2b5
robneu <rob@wpsitecare.com>
2016-01-29 21:15:14 -0500

    Reorganized code

--- includes/template-entry.php
+++ includes/template-entry.php
@@ -10,70 +10,6 @@
  */
 
 /**
- * Display a formatted markup block with information about the entry's terms.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Entry::get_entry_terms
- * @param  array $args a list of arguments to pass to the entry terms method.
- * @return void
- */
-function alpha_entry_terms( $args = array() ) {
-	echo carelib_get( 'template-entry' )->get_entry_terms( $args );
-}
-
-/**
- * Display either an excerpt or the content depending on what page the user is
- * currently viewing.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Entry::get_content
- * @return void
- */
-function alpha_content() {
-	echo carelib_get( 'template-entry' )->get_content();
-}
-
-/**
- * Remove all actions from THA entry hooks and filter the WordPress post
- * content to return null.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Entry::null_entry
- * @return void
- */
-function alpha_null_entry() {
-	carelib_get( 'template-entry' )->null_entry();
-}
-
-/**
- * Filter the WordPress content to null between the entry_content_before
- * and entry_content_after hook locations.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Entry::null_entry_content
- * @return void
- */
-function alpha_null_entry_content() {
-	carelib_get( 'template-entry' )->null_entry_content();
-}
-
-/**
- * Hookable wrapper around a filter to null the WordPress core post content.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Entry::null_the_content
- * @return void
- */
-function alpha_null_the_content() {
-	carelib_get( 'template-entry' )->null_the_content();
-}
-
-/**
  * Output opening markup for the entry article element.
  *
  * @since  0.1.0
@@ -253,6 +189,57 @@ function alpha_entry_content_open() {
 }
 
 /**
+ * Display either an excerpt or the content depending on what page the user is
+ * currently viewing.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Entry::get_content
+ * @return void
+ */
+function alpha_content() {
+	echo carelib_get( 'template-entry' )->get_content();
+}
+
+/**
+ * Remove all actions from THA entry hooks and filter the WordPress post
+ * content to return null.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Entry::null_entry
+ * @return void
+ */
+function alpha_null_entry() {
+	carelib_get( 'template-entry' )->null_entry();
+}
+
+/**
+ * Filter the WordPress content to null between the entry_content_before
+ * and entry_content_after hook locations.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Entry::null_entry_content
+ * @return void
+ */
+function alpha_null_entry_content() {
+	carelib_get( 'template-entry' )->null_entry_content();
+}
+
+/**
+ * Hookable wrapper around a filter to null the WordPress core post content.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Entry::null_the_content
+ * @return void
+ */
+function alpha_null_the_content() {
+	carelib_get( 'template-entry' )->null_the_content();
+}
+
+/**
  * Output singular entry pagination links.
  *
  * @since  0.1.0
@@ -307,6 +294,19 @@ function alpha_has_entry_footer() {
  */
 function alpha_entry_footer_open() {
 	echo '<footer ' . alpha_get_attr( 'entry-footer' ) . '>';
+}
+
+/**
+ * Display a formatted markup block with information about the entry's terms.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Entry::get_entry_terms
+ * @param  array $args a list of arguments to pass to the entry terms method.
+ * @return void
+ */
+function alpha_entry_terms( $args = array() ) {
+	echo carelib_get( 'template-entry' )->get_entry_terms( $args );
 }
 
 /**
