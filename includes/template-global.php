b3b99f39807921f69dda16f486ef5f0281ccdbb4
robneu <rob@wpsitecare.com>
2016-01-29 21:18:03 -0500

    Reorganized template global code

--- includes/template-global.php
+++ includes/template-global.php
@@ -44,110 +44,6 @@ function alpha_wrap_close() {
 }
 
 /**
- * Output a given menu template.
- *
- * @since  0.1.0
- * @access public
- * @param  string $name The name of the menu to load.
- * @uses   CareLib_Menu::template
- * @return void
- */
-function alpha_menu( $name = null ) {
-	carelib_get( 'menu' )->template( $name );
-}
-
-/**
- * Return the name of a given WordPress menu location.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Menu::get_location_name
- * @param  string $location The WordPress menu location to check.
- * @return string
- */
-function alpha_get_menu_location_name( $location ) {
-	return carelib_get( 'menu' )->get_location_name( $location );
-}
-
-/**
- * Display our breadcrumbs based on selections made in the WordPress customizer.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Global::display_breadcrumbs
- * @return bool true if both our template tag and theme mod return true.
- */
-function alpha_display_breadcrumbs() {
-	return carelib_get( 'template-global' )->display_breadcrumbs();
-}
-
-/**
- * Return a featured image using CareLib's advanced image grabber class.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Image_Grabber_API::get
- * @param  array $args a list of arguments to pass to the image grabber class.
- * @return mixed string or an array depending on what arguments are used
- */
-function alpha_get_image( $args = array() ) {
-	return carelib_get( 'image-grabber-api' )->get( $args );
-}
-
-/**
- * Display a featured image using CareLib's advanced image grabber class.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Image_Grabber_API::get
- * @param  array $args a list of arguments to pass to the image grabber class.
- * @return void
- */
-function alpha_image( $args = array() ) {
-	echo carelib_get( 'image-grabber-api' )->get( $args );
-}
-
-/**
- * Output a given sidebar template.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Sidebar::template
- * @param  string $name the name of the sidebar to load.
- * @return void
- */
-function alpha_sidebar( $name = null ) {
-	carelib_get( 'sidebar' )->template( $name );
-}
-
-/**
- * Return the name of a given dynamic sidebar.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Sidebar::get_name
- * @param  string $id The id of the sidebar to check.
- * @return string
- */
-function alpha_get_sidebar_name( $id ) {
-	return carelib_get( 'sidebar' )->get_name( $id );
-}
-
-/**
- * Return a formatted link to the customizer panel.
- *
- * @since  0.1.0
- * @access public
- * @uses   CareLib_Template_Global::get_customizer_link
- * @param  array $args options for how the link will be formatted.
- * @return string an escaped link to the WordPress customizer panel.
- */
-function alpha_get_customizer_link( $args = array() ) {
-	return carelib_get( 'template-global' )->get_customizer_link( $args );
-}
-
-
-/**
  * Output skip-to-content link markup for screen readers.
  *
  * @since  0.1.0
@@ -222,6 +118,32 @@ function alpha_branding_close() {
 }
 
 /**
+ * Output a given menu template.
+ *
+ * @since  0.1.0
+ * @access public
+ * @param  string $name The name of the menu to load.
+ * @uses   CareLib_Menu::template
+ * @return void
+ */
+function alpha_menu( $name = null ) {
+	carelib_get( 'menu' )->template( $name );
+}
+
+/**
+ * Return the name of a given WordPress menu location.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Menu::get_location_name
+ * @param  string $location The WordPress menu location to check.
+ * @return string
+ */
+function alpha_get_menu_location_name( $location ) {
+	return carelib_get( 'menu' )->get_location_name( $location );
+}
+
+/**
  * Output the markup for the primary menu toggle button.
  *
  * @since  0.1.0
@@ -278,6 +200,18 @@ function alpha_menu_secondary() {
 }
 
 /**
+ * Display our breadcrumbs based on selections made in the WordPress customizer.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Global::display_breadcrumbs
+ * @return bool true if both our template tag and theme mod return true.
+ */
+function alpha_display_breadcrumbs() {
+	return carelib_get( 'template-global' )->display_breadcrumbs();
+}
+
+/**
  * Load the breadcrumbs template.
  *
  * @since  0.1.0
@@ -298,6 +232,58 @@ function alpha_breadcrumbs() {
 }
 
 /**
+ * Return a featured image using CareLib's advanced image grabber class.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Image_Grabber_API::get
+ * @param  array $args a list of arguments to pass to the image grabber class.
+ * @return mixed string or an array depending on what arguments are used
+ */
+function alpha_get_image( $args = array() ) {
+	return carelib_get( 'image-grabber-api' )->get( $args );
+}
+
+/**
+ * Display a featured image using CareLib's advanced image grabber class.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Image_Grabber_API::get
+ * @param  array $args a list of arguments to pass to the image grabber class.
+ * @return void
+ */
+function alpha_image( $args = array() ) {
+	echo carelib_get( 'image-grabber-api' )->get( $args );
+}
+
+/**
+ * Output a given sidebar template.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Sidebar::template
+ * @param  string $name the name of the sidebar to load.
+ * @return void
+ */
+function alpha_sidebar( $name = null ) {
+	carelib_get( 'sidebar' )->template( $name );
+}
+
+/**
+ * Return the name of a given dynamic sidebar.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Sidebar::get_name
+ * @param  string $id The id of the sidebar to check.
+ * @return string
+ */
+function alpha_get_sidebar_name( $id ) {
+	return carelib_get( 'sidebar' )->get_name( $id );
+}
+
+/**
  * Load the primary sidebar on all multi-column layouts.
  *
  * @since  0.1.0
@@ -308,6 +294,19 @@ function alpha_primary_sidebar() {
 	if ( alpha_layout_has_sidebar() ) {
 		alpha_sidebar( 'primary' );
 	}
+}
+
+/**
+ * Return a formatted link to the customizer panel.
+ *
+ * @since  0.1.0
+ * @access public
+ * @uses   CareLib_Template_Global::get_customizer_link
+ * @param  array $args options for how the link will be formatted.
+ * @return string an escaped link to the WordPress customizer panel.
+ */
+function alpha_get_customizer_link( $args = array() ) {
+	return carelib_get( 'template-global' )->get_customizer_link( $args );
 }
 
 /**
