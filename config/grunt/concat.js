3239b71c770d21aa652d2f88de87c81c7e34fbe1
robneu <rob@wpsitecare.com>
2016-04-14 00:36:10 -0400

    Made js concat easier to expand

--- config/grunt/concat.js
+++ config/grunt/concat.js
@@ -1,11 +1,15 @@
 // https://github.com/gruntjs/grunt-contrib-concat
 module.exports = {
 	js: {
-		src: [
-			'<%= paths.bower %>/js/concat/**/*.js',
-			'<%= paths.authorAssets %>js/mobileMenu.js',
-			'<%= paths.authorAssets %>js/theme.js'
-		],
-		dest: 'js/theme.js'
+		files: [
+			{
+				src: [
+					'<%= paths.bower %>/js/concat/**/*.js',
+					'<%= paths.authorAssets %>js/mobileMenu.js',
+					'<%= paths.authorAssets %>js/theme.js'
+				],
+				dest: 'js/theme.js'
+			}
+		]
 	}
 };
