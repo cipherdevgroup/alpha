9dba40f800ec841e061631c406f34469406794a0
robneu <rob@wpsitecare.com>
2016-11-23 13:28:39 -0500

    Changed theme JS structure a bit

--- assets/theme/js/theme.js
+++ assets/theme/js/theme.js
@@ -4,25 +4,29 @@
  *
  * Includes all JS which is required within all sections of the theme.
  */
-(function( window, $, undefined ) {
+function AlphaParentTheme( $ ) {
 	'use strict';
 
-	var $document = $( document ),
-		$body     = $( document.body );
+	var $body = $( document.body );
 
-	$body.addClass( 'ontouchstart' in window || 'onmsgesturechange' in window ? 'touch' : 'no-touch' );
+	this.detectTouch = function() {
+		$body.addClass( 'ontouchstart' in window || 'onmsgesturechange' in window ? 'touch' : 'no-touch' );
+	};
 
-	// Global script initialization
-	function globalInit() {
-		var $siteInner = $( '#site-inner' );
-		$document.gamajoAccessibleMenu();
-		$( '#menu-primary' ).alphaMobileMenu();
-		$siteInner.fitVids();
-	}
+	this.init = function() {
+		var $siteInner = $( document.getElementById( 'menu-primary' ) );
 
-	// Document ready.
-	$document.ready(function() {
 		skipLinkFocus.init();
-		globalInit();
-	});
-})( this, jQuery );
+		$( document ).gamajoAccessibleMenu();
+		$( document.getElementById( 'menu-primary' ) ).alphaMobileMenu();
+		$siteInner.fitVids();
+	};
+}
+
+var AlphaParentTheme = new AlphaParentTheme( jQuery );
+
+AlphaParentTheme.detectTouch();
+
+jQuery( document ).ready(function() {
+	AlphaParentTheme.init();
+});
