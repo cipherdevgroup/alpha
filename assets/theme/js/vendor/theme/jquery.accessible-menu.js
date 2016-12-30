01cb0533096f2526dd88fa33835c40293a38fccd
robneu <rob@wpsitecare.com>
2016-12-29 20:25:32 -0500

    Added generated files

---
+++ assets/theme/js/vendor/theme/jquery.accessible-menu.js
@@ -0,0 +1,96 @@
+/*! Gamajo Accessible Menu - v1.0.0 - 2014-09-08
+* https://github.com/GaryJones/accessible-menu
+* Copyright (c) 2014 Gary Jones; Licensed MIT */
+;(function( $, window, document, undefined ) {
+  'use strict';
+
+  var pluginName = 'gamajoAccessibleMenu',
+    hoverTimeout = [];
+
+  // The actual plugin constructor
+  function Plugin( element, options ) {
+    this.element = element;
+    // jQuery has an extend method which merges the contents of two or
+    // more objects, storing the result in the first object. The first object
+    // is generally empty as we don't want to alter the default options for
+    // future instances of the plugin
+    this.opts = $.extend( {}, $.fn[pluginName].options, options );
+    this.init();
+  }
+
+  // Avoid Plugin.prototype conflicts
+  $.extend( Plugin.prototype, {
+    init: function() {
+      $( this.element )
+        .on( 'mouseenter.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemEnter )
+        .on( 'mouseleave.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemLeave )
+        .find( 'a' )
+        .on( 'focus.'  + pluginName + ', blur.' + pluginName, this.opts, this.menuItemToggleClass );
+    },
+
+    /**
+     * Add class to menu item on hover so it can be delayed on mouseout.
+     *
+     * @since 1.0.0
+     */
+    menuItemEnter: function( event ) {
+      // Clear all existing hover delays
+      $.each( hoverTimeout, function( index ) {
+        $( '#' + index ).removeClass( event.data.hoverClass );
+        clearTimeout( hoverTimeout[index] );
+      });
+      // Reset list of hover delays
+      hoverTimeout = [];
+
+      $( this ).addClass( event.data.hoverClass );
+    },
+
+    /**
+     * After a short delay, remove a class when mouse leaves menu item.
+     *
+     * @since 1.0.0
+     */
+    menuItemLeave: function( event ) {
+      var $self = $( this );
+      // Delay removal of class
+      hoverTimeout[this.id] = setTimeout( function() {
+        $self.removeClass( event.data.hoverClass );
+      }, event.data.hoverDelay );
+    },
+
+    /**
+     * Toggle menu item class when a link fires a focus or blur event.
+     *
+     * @since 1.0.0
+     */
+    menuItemToggleClass: function( event ) {
+      $( this ).parents( event.data.menuItemSelector ).toggleClass( event.data.hoverClass );
+    }
+  });
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[ pluginName ] = function( options ) {
+    this.each( function() {
+      if ( ! $.data( this, 'plugin_' + pluginName ) ) {
+        $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+      }
+    });
+
+    // chain jQuery functions
+    return this;
+  };
+
+  $.fn[ pluginName ].options = {
+    // The CSS class to add to indicate item is hovered or focused
+      hoverClass: 'menu-item-hover',
+
+      // The delay to keep submenus showing after mouse leaves
+      hoverDelay: 250,
+
+      // Selector for general menu items. If you remove the default menu item
+      // classes, then you may want to call this plugin with this value
+      // set to something like 'nav li' or '.menu li'.
+      menuItemSelector: '.menu-item'
+  };
+})( jQuery, window, document );
---
+++ assets/theme/js/vendor/theme/skip-link-focus.js
@@ -0,0 +1,70 @@
+/**
+ * Skip Link Focus v0.1.0
+ * https://github.com/cedaro/skip-link-focus
+ *
+ * @copyright Modifications Copyright (c) 2015 Cedaro, LLC
+ * @license BSD-3-Clause
+ */
+
+/**
+ * Make "skip to content" links work correctly in IE9, Chrome, and Opera to
+ * improve accessibility.
+ *
+ * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+ */
+(function( root, factory ) {
+	'use strict';
+
+	if ( 'function' === typeof define && define.amd ) {
+		define( [], factory );
+	} else if ( 'object' === typeof exports ) {
+		module.exports = factory();
+	} else {
+		root.skipLinkFocus = factory();
+	}
+}( this, function() {
+	'use strict';
+
+	function init() {
+		if ( window && /webkit|opera|msie/i.test( window.navigator.userAgent ) && window.addEventListener ) {
+			var i,
+				skipLinks = window.document.querySelectorAll( '.skip-link' );
+
+			window.addEventListener( 'hashchange', function() {
+				skipToElement( location.hash.substring( 1 ) );
+			}, false );
+
+			// Fix for when the address bar already contains a hash.
+			for ( i = 0; i < skipLinks.length; ++i ) {
+				skipLinks[ i ].addEventListener( 'click', skipLinkClickHandler );
+			}
+		}
+	}
+
+	function skipLinkClickHandler( e ) {
+		skipToElement( e.target.hash.substring( 1 ) );
+	}
+
+	function skipToElement( id ) {
+		var element;
+
+		if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+			return;
+		}
+
+		element = window.document.getElementById( id );
+
+		if ( element ) {
+			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+				element.tabIndex = -1;
+			}
+
+			element.focus();
+		}
+	}
+
+	return {
+		init: init,
+		skipToElement: skipToElement
+	};
+}));
