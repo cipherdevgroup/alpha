67dd180c8665d684fb2b6353c5354803c5a4452f
robneu <rob@wpsitecare.com>
2016-12-29 23:06:35 -0500

    Cleaned up single product template

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -8,14 +8,24 @@
  */
 
 /**
- * Callback defined in includes/template-entry.php
+ * Callback defined in carelib/includes/template-entry.php
  *
  * @see carelib_null_entry_containers
  */
 add_action( 'carelib_content_while_before', 'carelib_null_entry_containers', 10 );
 
-add_action( 'carelib_content_before', 'alpha_wc_before_main_content', 8 );
+/**
+ * Callback defined in carelib/includes/woocommerce/template-hooks.php
+ *
+ * @see carelib_wc_before_main_content
+ */
+add_action( 'carelib_content_before', 'carelib_wc_before_main_content', 8 );
 
-add_action( 'carelib_content_after',  'alpha_wc_after_main_content', 8 );
+/**
+ * Callback defined in carelib/includes/woocommerce/template-hooks.php
+ *
+ * @see carelib_wc_after_main_content
+ */
+add_action( 'carelib_content_after', 'carelib_wc_after_main_content', 8 );
 
 carelib_framework( 'shop' );
